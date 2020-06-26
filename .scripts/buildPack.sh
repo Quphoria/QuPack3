ef90667d8f48aff23730be1d1e23064cd4269e53
Quphoria <s.simpson41.ss@googlemail.com>
2020-06-26 11:10:11 +0100

    Remove \r from url

--- .scripts/buildPack.sh
+++ .scripts/buildPack.sh
@@ -32,7 +32,7 @@ function download_file {
         #progress_flag='-s'
         progress_flag='-#'
     fi
-    echo "\"$download_url\""
+    download_url=${download_url%$'\r'} # Remove \r from url
     curl -O -J -L --globoff --compressed $progress_flag "$download_url" || (echo "Failed to download $download_url" && exit 1)
 }
 
